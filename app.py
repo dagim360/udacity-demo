b138b1a831b764a7f6d9ca01f97231cba2a9d70e
dagim360 <68782461+dagim360@users.noreply.github.com>
2022-05-24 19:41:12 -0700

    new commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app  = Flask(__name__)
@@ -7,13 +8,25 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+
+    return jsonify({
+        'description': todo.description
+    })
+
 @app.route('/')
 
 def index():
